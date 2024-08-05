295d8c922b31ca69e5685304be7c8aa5080111bb
kyurkov <opcheese@gmail.com>
2024-08-05 14:16:47 -0400

    nft1

--- sources/contract.deploy.ts
+++ sources/contract.deploy.ts
@@ -9,9 +9,10 @@ import { createOffchainContent } from "./helpers";
     // Parameters
     let testnet = true;
     let packageName = "nft_NftCollection.pkg";
-    let owner = Address.parse("0QCNGf2fxCJx7sFH83JUayIFh9Y5a9KG8rG-6wDV1r1gW_ij");
+    let owner = Address.parse("0QCNGf2fxCJx7sFH83JUayIFh9Y5a9KG8rG-6wDV1r1gW_ij"); //kQAJZdY7CZgzeH-G_6hFoU6qyLOVNLbHCfRUGUJOBbE8qaIS 
+    //let owner = Address.parse("EQAJZdY7CZgzeH-G_6hFoU6qyLOVNLbHCfRUGUJOBbE8qRmY"); //kQDMIqppCpbXwBYg1ngXHzrt2L3_pcYSxbycl-zYJE-5uijZ
     let content = createOffchainContent("https://raw.githubusercontent.com/opcheese/tactexps/main/collection.json");
-    let init = await NftCollection.init(owner, content, owner, 21n, 1000n);
+    let init = await NftCollection.init(owner, content, owner, 11n, 1000n); //EQCXnq1SWEa-poYLriArmVN79tFspQ2bw1AZbar1Uxs6Gxsz
 
     // Load required data
     let address = contractAddress(0, init);
--- sources/contract.spec.ts
+++ sources/contract.spec.ts
@@ -1,15 +1,56 @@
 import { beginCell, toNano } from "@ton/core";
 import { ContractSystem } from "@tact-lang/emulator";
-import { NFTItem } from "./output/nft_NFTItem";
+import { NftCollection } from "./output/nft_NftCollection";
 import { inspect } from "util";
 import exp from "constants";
 
 describe("contract", () => {
-  it("should deploy correctly", async () => {
+  // it("should deploy correctly", async () => {
+  //   let system = await ContractSystem.create();
+  //   let owner = system.treasure("owner");
+  //   let master = system.treasure("master");
+  //   let nftContract = system.open(await NFTItem.fromInit(master.address, 0n, beginCell().storeUint(2, 8).endCell(), owner.address, master.address, 5n, 100n));
+
+  //   let nft_tracker = system.track(nftContract.address);
+
+  //   await nftContract.send(master, { value: toNano(0.1) }, { $$type: "Deploy", queryId: 0n });
+  //   await system.run();
+
+  //   let nft_events = nft_tracker.collect();
+  //   console.log(inspect(nft_events, true, null, true));
+
+  //   let result = await nftContract.getGetNftData();
+  //   expect(result.deployed).toBeTruthy();
+  //   expect(result.index).toEqual(0n);
+  //   expect(result.collection.equals(master.address)).toBeTruthy();
+  //   expect(result.owner.equals(owner.address)).toBeTruthy();
+  //   expect(result.content.toBoc()).toEqual(beginCell().storeUint(2, 8).endCell().toBoc());
+
+
+  //   let owner2 = system.treasure("owner2");
+  //   await nftContract.send(owner, { value: toNano(1) }, {
+  //     $$type: "NftTransfer", query_id: 1n, new_owner: owner2.address, response_destination: owner.address,
+  //     custom_payload: beginCell().storeUint(0, 32).storeStringTail("Test message 1").endCell(),
+  //     forward_amount: toNano(0.05),
+  //     forward_payload: beginCell().storeUint(0, 32).storeStringTail("Test message 2").endCell()
+  //   });
+  //   await system.run();
+
+  //   let owner_events = nft_tracker.collect();
+  //   console.log(inspect(owner_events, true, null, true));
+
+  //   result = await nftContract.getGetNftData();
+
+  //   expect(result.owner.equals(owner2.address)).toBeTruthy();
+
+  // });
+
+
+  it("should deploy collection", async () => {
     let system = await ContractSystem.create();
     let owner = system.treasure("owner");
     let master = system.treasure("master");
-    let nftContract = system.open(await NFTItem.fromInit(master.address, 0n, beginCell().storeUint(2, 8).endCell(), owner.address, master.address, 5n, 100n));
+    let nftContract = system.open(await NftCollection.fromInit(master.address, 0n, beginCell().storeUint(2, 8).endCell(), owner.address, master.address, 5n, 100n));
 
     let nft_tracker = system.track(nftContract.address);
 
